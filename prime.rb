6d05aaed5665bddea0550b8bda7cf67e308f2a74
Xiaoyi Liu <lxy878@gmail.com>
2020-03-05 15:35:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+def prime?(number)
+  if number < 2
+    return false
+  end
+  if number == 2 or number == 3
+    return true
+  end
+  if number % 2 == 0
+    return false
+  end
+  i = 5
+  while i < number
+    if number % i == 0
+      return false
+    end
+    i +=2
+  end
+  return true
+end
