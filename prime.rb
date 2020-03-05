33e257ab266ed7bd76598e25a394629d01c4adba
Xiaoyi Liu <lxy878@gmail.com>
2020-03-05 15:40:56 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -9,12 +9,12 @@ def prime?(number)
   if number % 2 == 0
     return false
   end
-  i = 5
+  i = 2
   while i < number
     if number % i == 0
       return false
     end
-    i +=2
+    i +=1
   end
   return true
 end
